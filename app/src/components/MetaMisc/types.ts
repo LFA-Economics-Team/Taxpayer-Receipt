a147bd00fb58e1db8249ae9afac1658fa4fc341e
Jared <jgib5432@gmail.com>
2026-04-17 16:49:23 -0600

    daily work 4-17-2026

--- app/src/components/MetaMisc/types.ts
+++ app/src/components/MetaMisc/types.ts
@@ -11,6 +11,19 @@ export const filingOptions = [
   },
 ];
 
+export const dependentOptions = [
+  { value: 1, label: 1 },
+  { value: 2, label: 2 },
+  { value: 3, label: 3 },
+  { value: 4, label: 4 },
+  { value: 5, label: 5 },
+  { value: 6, label: 6 },
+  { value: 7, label: 7 },
+  { value: 8, label: 8 },
+  { value: 9, label: 9 },
+  { value: 10, label: "10 or more" },
+];
+
 // Sales Tax
 
 export type SalesLocation = {
